c8fd69eb7a7814c96530c536e31c67e7a8a54574
Tony Gaskell <thgaskell@gmail.com>
2015-10-10 12:43:26 -1000

    Import top 10 global trends on startup

--- server/server.js
+++ server/server.js
@@ -1,18 +1,12 @@
-Meteor.startup(function () {
-  if (Hashtags.find().count() === 0) {
-    var trendingHashtags = [
-      {
-        hashtag: '#selfie'
-      },
-      {
-        hashtag: '#builtwithmeteor'
-      },
-      {
-        hashtag: '#devleague'
-      }
-    ];
-    for (var i = 0; i < trendingHashtags.length; i++){
-      Hashtags.insert(trendingHashtags[i]);
-    }
-  }
-});
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // Create twitter api instance
+    Twitter = new TwitMaker(Meteor.settings.twitterAPI);
+
+    // Clear trends on startup
+    Trends.remove({});
+
+    // Fetch current trends
+    insertTrends();
+  });
+}
