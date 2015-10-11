aa21f9f08d0dcaafdccb6a1aff5926fa5a818da8
Tony Gaskell <thgaskell@gmail.com>
2015-10-10 16:11:13 -1000

    Format timestamps

--- client/views/chatroom/chatroom.js
+++ client/views/chatroom/chatroom.js
@@ -2,6 +2,35 @@
 Template.chatroom.created = function () {
 };
 
+Template.chatroom.rendered = function () {
+
+};
+
 // Already in data context
 Template.chatroom.helpers({
+});
+
+
+// Already in blaze template instance
+Template.tweet.created = function () {
+};
+
+Template.tweet.rendered = function () {
+};
+
+// Already in data context
+Template.tweet.helpers({
+  timestamp: function () { return moment(this.timestamp).fromNow(); }
+});
+
+// Already in blaze template instance
+Template.message.created = function () {
+};
+
+Template.message.rendered = function () {
+};
+
+// Already in data context
+Template.message.helpers({
+  timestamp: function () { return moment(this.timestamp).fromNow(); }
 });
